03b807ded54e9e3ba44afb0fbca6b99b0f6fcaac
Marlon Morris <mmorris@Marlons-MacBook-Pro.local>
2018-08-26 18:53:45 -0600

    adding more applicaiton files and a new test

--- test/first_spec.js
+++ test/first_spec.js
@@ -3,6 +3,7 @@ var assert = require("assert");
 describe("A feature",function(){
   describe("A scenario", function(){
     it("does something expected", function(){
+      //TODO: add some code here
       assert(true);
     });
   });
